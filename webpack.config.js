e0be4b3bf2de4148e61d59ffab76fab91984e982
shinework <adrien.baptiste@gmail.com>
2017-02-11 00:26:44 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,41 @@ var webpack = require('webpack'),
   path = require('path'),
   srcPath = path.join(__dirname, 'src');
 
-var isProduction = function () {
-  return process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
+
+var plugins = [
+  new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+  new webpack.optimize.UglifyJsPlugin({
+    mangle: {
+      except: ['$super', '$', 'exports', 'require']
+    },
+    compress: {
+      warnings: false
+    }
+  }),
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: 'src/index.html',
+    minify: true,
+    favicon: 'favicon-96x96.png'
+  }),
+  new webpack.NoErrorsPlugin()
+];
+
+if (isProduction) {
+  plugins = plugins.concat([
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false
+      }
+    }),
+  ]);
 }
 
 module.exports = {
@@ -39,27 +72,9 @@ module.exports = {
       { test: /\.json$/, loader: "json" }
     ]
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: {
-        except: ['$super', '$', 'exports', 'require']
-      },
-      compress: {
-        warnings: false
-      }
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: 'src/index.html',
-      minify: true,
-      favicon: 'favicon-96x96.png'
-    }),
-    new webpack.NoErrorsPlugin()
-  ],
-
-  debug: isProduction() ? false : true,
-  devtool: isProduction() ? '' : 'eval-cheap-module-source-map',
+  plugins: plugins,
+  debug: !isProduction,
+  devtool: isProduction ? '' : 'eval-cheap-module-source-map',
   devServer: {
     contentBase: './tmp',
     historyApiFallback: true
